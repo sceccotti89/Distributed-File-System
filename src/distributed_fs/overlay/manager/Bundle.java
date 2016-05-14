83b687a6abfe9b516c6e7b5c2f5af0ca53e145e4
Stefano <Stefano@192.168.1.102>
2016-05-14 19:37:09 +0200

    Refactoring.

---
+++ src/distributed_fs/overlay/manager/Bundle.java
@@ -0,0 +1,10 @@
+package distributed_fs.overlay.manager;
+
+import java.util.HashMap;
+
+public class Bundle<T> extends HashMap<String, T>
+{
+	private static final long serialVersionUID = 368098630821565429L;
+	
+	
+}
