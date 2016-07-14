4dc696d7aa8fbf494707385341576cd3f20f955d
stefano <stefano@stefano>
2016-07-14 18:26:24 +0200

    Removed test sections.

--- server/src/main/java/distributed_fs/NodeLauncher.java
+++ server/src/main/java/distributed_fs/NodeLauncher.java
@@ -23,10 +23,7 @@ public class NodeLauncher
 {
     public static void main( final String args[] ) throws ParseException, IOException, InterruptedException, DFSException
     {
-        //NodeArgsParser.parseArgs( args );
-        NodeArgsParser.parseArgs( new String[]{ "-t", "0", "--help" } );
-        //NodeArgsParser.parseArgs( new String[]{ "-t", "1", "-v", "10", "--port", "8000", "-n", "127.0.0.1:3000:1" } );
-        //NodeArgsParser.parseArgs( new String[]{ "-t", "1", "-f", "Settings/NodeSettings.json" } );
+        NodeArgsParser.parseArgs( args );
         if(NodeArgsParser.hasOnlyHelpOptions())
             return;
         
