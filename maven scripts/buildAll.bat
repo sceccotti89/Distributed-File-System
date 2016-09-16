4655a41fd4ddf1e825e5eee57bb954f15718c229
Stefano <Stefano@192.168.1.100>
2016-09-16 10:37:40 +0200

    Fixed the install-file command for Windows.

--- maven scripts/buildAll.bat
+++ maven scripts/buildAll.bat
@@ -2,8 +2,7 @@ cd ../gossip
 call mvn install
 
 cd ../server
-call mvn install:install-file -Dfile=libs/mr-udp-1.0.jar -DgroupId=rudp \
-    -DartifactId=mr-udp -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=libs/mr-udp-1.0.jar -DgroupId=rudp -DartifactId=mr-udp -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install
 
 cd../client
